cf923ea897551f9a6094ccab369f2bc88685489e
Ariq Dhia Irfanudin <invanteris@gmail.com>
2021-02-01 17:06:53 +0700

    Connecting to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+	"mongodb+srv://ariqshd:!sAKURAS0U@menariq.raw5o.mongodb.net/<dbname>?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+	console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+	console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
 	res.send("Hi there");
