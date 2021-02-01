7380c0cf6c8bada665a155f7962c47c961247d11
Ariq Dhia Irfanudin <invanteris@gmail.com>
2021-02-01 17:27:44 +0700

    Making a post request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri =
 	"mongodb+srv://ariqshd:!sAKURAS0U@menariq.raw5o.mongodb.net/<dbname>?retryWrites=true&w=majority";
